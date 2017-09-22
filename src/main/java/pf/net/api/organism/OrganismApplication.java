21b18f4b7c48950faf1c90b95db6d3de1e0b8282
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2017-09-21 18:34:53 -1000

    Add Eureka client

--- src/main/java/pf/net/api/organism/OrganismApplication.java
+++ src/main/java/pf/net/api/organism/OrganismApplication.java
@@ -2,8 +2,10 @@ package pf.net.api.organism;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrganismApplication {
 
 	public static void main(String[] args) {
